e9dc42e7be8317309e2fc4018f6d22fe74a8ff1d
Jonas Lundahl <jonas.lundahl@outlook.com>
2023-12-16 10:15:41 +0100

    Select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,58 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <StyledSelect value={value} onChange={onChange}>
+        {children}
+      </StyledSelect>
+      <Text>{displayedValue}</Text>
+      <StyledIcon id="chevron-down" />
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  padding: 12px 16px;
+  position: relative;
+  border-radius: 8px;
+  background-color: ${COLORS.transparentGray15};
+  width: fit-content;
+
+  &:hover {
+    color: ${COLORS.gray700};
+  }
+
+  &:focus-within {
+    outline: -webkit-focus-ring-color auto 1px;
+  }
+`;
+
+const Text = styled.span`
+  margin-right: 16px;
+  vertical-align: middle;
+`;
+
+const StyledIcon = styled(Icon)`
+  display: inline-block;
+  vertical-align: middle;
+`;
+
+const StyledSelect = styled.select`
+  position: absolute;
+  inset: 0;
+  appearance: none;
+  border: none;
+  background-color: transparent;
+  outline: none;
+  color: transparent;
+`;
 
 export default Select;
